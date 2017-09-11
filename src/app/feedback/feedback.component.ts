2b35523f139d6b2d12b287074e4b84d91d53c32c
Agrawala <neha.agrawala@capgemini.com>
2017-09-11 17:44:31 +0530

    update

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
+ myForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+    this.myForm = formBuilder.group({
+      'username': ['', [Validators.required]],
+      'email': ['', [Validators.required]],
+      'comment' :['', [Validators.required]]
+      
+    })
   }
 
+  ngOnInit() {
+    if(this.authService.isAuthenticated()) {
+      this.router.navigate(['/feedback']);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.myForm.reset();
+  }
+
+  Submit() {
+    this.authService.signinUser(this.myForm.value);
+    if(this.authService.isAuthenticated()) {
+      this.router.navigate(['/home']);
+    }
+  }
 }
--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -5,4 +5,5 @@ export class User {
     public username: string;
     public email: string;
     public gender: string;
+    public comment :string;
 }
